2eff832294ed3558ad6772309f405281c48d7b27
Paul <appcellon@gmail.com>
2020-04-27 00:08:48 +0300

    dropping text timing changed

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ const Home = () => (
       <div class="grid">
 
         <h1>
-          <div>Experts in </div>
+          Elegance Defined in
           <div class="dropping-texts">
             <div>Software Development</div>
             <div>DevOPs Engineering</div>
