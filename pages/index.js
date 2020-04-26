d04bbd41ca88cf71a81cf7a9694ad286b5cfa053
Paul <appcellon@gmail.com>
2020-04-26 20:06:22 +0300

    added new post

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,14 @@ const Home = () => (
 
       <div class="grid">
 
-        <h1>Elegance defined Software</h1>
+        <h1>Elegance defined Software
+          <div class="dropping-texts">
+            <div>Developers</div>
+            <div>Designers</div>
+            <div>Coders</div>
+            <div>EVERYONE!</div>
+          </div>
+        </h1>
 
         <a href="#" class="button--scroll animation animation--infinite animation--up-down"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="-275 398.7 44.2 44.2"><path fill="#fff" d="M-275 420.8c0-12.2 9.9-22.1 22.1-22.1s22.1 9.9 22.1 22.1c0 12.2-9.9 22.1-22.1 22.1s-22.1-9.9-22.1-22.1zm42.7 0c0-11.4-9.2-20.6-20.6-20.6-11.4 0-20.6 9.2-20.6 20.6 0 11.4 9.2 20.6 20.6 20.6 11.4 0 20.6-9.3 20.6-20.6zM-260.6 418c0-.2.1-.4.2-.5.3-.3.8-.3 1.1 0l6.8 6.8 6.8-6.8c.3-.3.8-.3 1.1 0 .3.3.3.8 0 1.1l-7.3 7.3c-.3.3-.8.3-1.1 0l-7.3-7.3c-.3-.2-.3-.4-.3-.6z"/></svg></a>
 
